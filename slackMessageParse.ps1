d9faa6b646d71cbc01d937eda54aaf914ab6e975
Rafael Heringer <rafael@nibo.com.br>
2021-04-29 16:37:08 -0300

    Update slackMessageParse.ps1

--- slackMessageParse.ps1
+++ slackMessageParse.ps1
@@ -1,3 +1,4 @@
 $PSDefaultParameterValues = @{ '*:Encoding' = 'utf8' }
-$fileContent = (Get-Content("$(System.DefaultWorkingDirectory)\SlackNotesTemplate.md") -join "`n")
+$fileContent = Get-Content("$(System.DefaultWorkingDirectory)\SlackNotesTemplate.md")
+$fileContent = $fileContent.replace("`n","``n")
 Write-Output "##vso[task.setvariable variable=slackMessage]$fileContent"
