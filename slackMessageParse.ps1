67764da60bd1541777fa43aa9db0043f698be34b
Rafael Heringer <rafael@nibo.com.br>
2021-04-29 16:43:34 -0300

    Delete slackMessageParse.ps1

--- slackMessageParse.ps1
+++
@@ -1,4 +0,0 @@
-$PSDefaultParameterValues = @{ '*:Encoding' = 'utf8' }
-$fileContent = Get-Content("$(System.DefaultWorkingDirectory)\SlackNotesTemplate.md")
-$fileContent = $fileContent.replace("`n","``n")
-Write-Output "##vso[task.setvariable variable=slackMessage]$fileContent"
